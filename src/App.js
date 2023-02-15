67660f51d47a9f6d4b2cc55c86015ca89773d82e
Canan Ağbal <agbalcanan8069@gmail.com>
2023-02-15 17:32:38 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-import data from "./helper/data"
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       
     
     </div>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css"
+import İmg from "../../assets/clarusway.png"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <img src={İmg} alt="" />
+      
+    </div>
+  )
+}
+
+export default Navbar
